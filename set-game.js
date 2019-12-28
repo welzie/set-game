14de70dcabb9cfb9b775397368a3b242c1e05bd6
Blake Mills <welzie@gmail.com>
2019-12-28 14:38:07 -0600

    reworkd isSet logic

--- set-game.js
+++ set-game.js
@@ -1,72 +1,79 @@
 function createCards() {
-    const cards = [];
-    const properties = {
-        color: ['purple', 'green', 'red'],
-        shape: ['oval', 'squiqgle', 'diamond'],
-        fill: ['empty', 'shaded', 'solid'],
-        count: [1, 2, 3]
-    };
+  const cards = [];
+  const properties = {
+    color: ['purple', 'green', 'red'],
+    shape: ['oval', 'squiqgle', 'diamond'],
+    fill: ['empty', 'shaded', 'solid'],
+    count: [1, 2, 3]
+  };
 
-    let number = 0;
-    properties.color.forEach( color => {
-        properties.shape.forEach( shape => {
-            properties.fill.forEach( fill => {
-                properties.count.forEach( count => {
-                    cards.push({ id: ++number, color, shape, fill,  count});
-                })
-            });
-        });
+  let number = 0;
+  properties.color.forEach(color => {
+    properties.shape.forEach(shape => {
+      properties.fill.forEach(fill => {
+        properties.count.forEach(count => {
+          cards.push({ id: ++number, color, shape, fill, count });
+        })
+      });
     });
-    return cards;
+  });
+  return cards;
 };
 
 /*
  * Warning this mutates the cards array
  */
 function drawRandomCards(cards, numberToDraw) {
-    const randomCards = [];
-    for (let i=0; i < numberToDraw; i++) {
-        const randomIndex = getRandomInt(cards.length - 1);
-        const drawnCardArray = cards.splice(randomIndex, 1);
-        randomCards.push(drawnCardArray[0]);
-    }
-    return randomCards;
+  const randomCards = [];
+  for (let i = 0; i < numberToDraw; i++) {
+    const randomIndex = getRandomInt(cards.length - 1);
+    const drawnCardArray = cards.splice(randomIndex, 1);
+    randomCards.push(drawnCardArray[0]);
+  }
+  return randomCards;
 }
 
 function getRandomInt(max) {
-    //TODO: is this able to return 0?
-    return Math.floor(Math.random() * Math.floor(max));
+  //TODO: is this able to return 0?
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
+/**
+ * Should return an array of arrays with the inner arrays being unique sets.
+ */
 function findSets(availableCards) {
-    const sets = [];
-    if (availableCards.length > 2) {
-        const c1 = availableCards[0];
-        const remainingAvailableCards = availableCards.slice(1);
-        const lowerSets = findSets(remainingAvailableCards);
-        sets.concat(lowerSets || []);
-        for (let x=1; x < remainingAvailableCards.length; x++) {
-            const c2 = remainingAvailableCards[x];
-            for (let y=x+1; y < remainingAvailableCards.length; y++) {
-                c3 = remainingAvailableCards[y];
-                const possibleSet = [c1, c2, c3];
-                if (isSet(possibleSet)) {
-                    sets.push(possibleSet);
-                }
-            }
-        }
-        return sets;
-    } else {
-        return false;
-    }
+  const sets = [];
+  return sets;
 }
 
-function isSet(c1, c2, c3) {
-    return true;
+/**
+ * Returns true if
+ * - Each property is the same OR different for each card
+ * - The properties can be all different, all the same, or a mix
+ * Properties: ['color', 'shape', 'fill', 'count']
+ * @param {card object} card1 
+ * @param {card object} card2 
+ * @param {card object} card3 
+ */
+function isSet(card1, card2, card3) {
+  const properties = ['color', 'shape', 'fill', 'count'];
+  const results = {};
+  properties.forEach( property => {
+    if (card1[property] === card2[property] && card2[property] === card3[property]) {
+      results[property] = 'equal';
+    } else if (card1[property] !== card2[property] && card1[property] !== card3[property] && card2[property] !== card3[property]) {
+      results[property] = 'different';
+    } else {
+      results[property] = 'invalid';
+    }
+  });
+  //console.log(results);
+  return Object.values(results).indexOf('invalid') < 0;
 }
 
 module.exports = {
-    createCards,
-    drawRandomCards,
-    findSets
+  createCards,
+  drawRandomCards,
+  findSets,
+  isSet
 };
--- spec/set-basics-spec.js
+++ spec/set-basics-spec.js
@@ -1,21 +1,54 @@
 const assert = require('assert');
-const { createCards, drawRandomCards, findSets } = require('../set-game.js');
+const { createCards, drawRandomCards, findSets, isSet } = require('../set-game.js');
 
-describe("card mechanics work", function() {
+describe("set logic", function() {
 
   it("creates deck", function(){
     const cards = createCards();
-    assert.strictEqual(81, cards.length);
+    assert.strictEqual(cards.length, 81);
     // TODO: validate that correct amount of each type were created
   });
 
   it("drawing random cards", function() {
     const deck = createCards();
     const availableCards = drawRandomCards(deck, 12);
-    assert.strictEqual(12, availableCards.length);
-    assert.strictEqual(69, deck.length);
+    assert.strictEqual(availableCards.length, 12);
+    assert.strictEqual(deck.length, 69);
   });
 
-  
+  it("correct determines if cards differing by one property are set", function() {
+    const cards = validSets.differInCount;
+    assert.strictEqual(isSet(cards[0], cards[1], cards[2]), true);
+  });
+
+  it("two cards matching on a property are not set", function() {
+    const cards = inValidSets.twoHaveSameColor;
+    assert.strictEqual(isSet(cards[0], cards[1], cards[2]), false);
+  });
+
+  const validSets = {
+    // color: ['purple', 'green', 'red'],
+    // shape: ['oval', 'squiqgle', 'diamond'],
+    // fill: ['empty', 'shaded', 'solid'],
+    // count: [1, 2, 3]
+    differInCount: [
+      { id: 1, color: 'red', shape: 'oval', fill: 'solid',  count: 1},
+      { id: 2, color: 'red', shape: 'oval', fill: 'solid',  count: 2},
+      { id: 3, color: 'red', shape: 'oval', fill: 'solid',  count: 3}
+    ],
+    differInCountAndColor: [
+      { id: 1, color: 'red', shape: 'oval', fill: 'solid',  count: 1},
+      { id: 2, color: 'green', shape: 'oval', fill: 'solid',  count: 2},
+      { id: 3, color: 'purple', shape: 'oval', fill: 'solid',  count: 3}
+    ]
+  };
+
+  const inValidSets = {
+    twoHaveSameColor: [
+      { id: 1, color: 'red', shape: 'oval', fill: 'solid',  count: 1},
+      { id: 2, color: 'red', shape: 'oval', fill: 'solid',  count: 2},
+      { id: 3, color: 'purple', shape: 'oval', fill: 'solid',  count: 3}
+    ]
+  }; 
 
 });
